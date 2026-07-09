169a2396740d92f0fd17956709e08ecec8775efe
Max Lindqvist <max.lindqvist@ipp.mpg.de>
2026-07-09 13:18:35 +0200

    Fix page links

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,3 +1,6 @@
 import { defineConfig } from "astro/config";
 
-export default defineConfig({});
+export default defineConfig({
+  site: "https://struphy-hub.github.io",
+  base: "/profiling-data",
+});
